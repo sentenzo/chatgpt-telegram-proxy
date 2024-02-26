8d372a8c41c295e77496d0664d473e5918c8efab
sentenzo <sentenzo@users.noreply.github.com>
2024-02-26 17:21:02 +0300

    `telethon` dummy bot

--- oaiapi_teleproxy/__main__.py
+++ oaiapi_teleproxy/__main__.py
@@ -1,7 +1,8 @@
 # flake8: noqa: F401
 
-from oaiapi_teleproxy.experiments import agr_main, ptb_main, tbt_main
+from oaiapi_teleproxy.experiments import agr_main, ptb_main, tbt_main, ttn_main
 
 # ptb_main()
 # agr_main()
-tbt_main()
+# tbt_main()
+ttn_main()
---
+++ oaiapi_teleproxy/experiments/telethon/bot.py
@@ -0,0 +1,26 @@
+import os
+
+from telethon import TelegramClient, events
+from telethon.tl.custom.message import Message
+
+TOKEN = os.environ.get("TELEGRAM_BOT_API_TOKEN", default="")
+API_ID = int(os.environ.get("TELEGRAM_APP_API_ID", default="0"))
+API_HASH = os.environ.get("TELEGRAM_APP_API_HASH", default="")
+
+bot = TelegramClient("telethon_sessions/bot", API_ID, API_HASH)
+bot.start(bot_token=TOKEN)
+
+
+@bot.on(events.NewMessage(pattern="/start"))
+async def start(event: Message) -> None:
+    await event.respond("Hello from `telethon` lib!")
+    raise events.StopPropagation
+
+
+@bot.on(events.NewMessage)
+async def echo(event: Message) -> None:
+    await event.respond(event.text)
+
+
+def main() -> None:
+    bot.run_until_disconnected()
