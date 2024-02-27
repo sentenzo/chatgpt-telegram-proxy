eb9139bf0028033b335a9fb49ebe4ba7c7932160
sentenzo <sentenzo@users.noreply.github.com>
2024-02-27 08:04:50 +0300

    trying out OpenAI API

--- oaiapi_teleproxy/__main__.py
+++ oaiapi_teleproxy/__main__.py
@@ -1,3 +1,5 @@
-from oaiapi_teleproxy.telegram.bot import launch_telegram_bot
+# from oaiapi_teleproxy.telegram.bot import launch_telegram_bot  # noqa: F401
+from oaiapi_teleproxy.openai.check import check_openai  # noqa: F401
 
-launch_telegram_bot()
+check_openai()
+# launch_telegram_bot()
