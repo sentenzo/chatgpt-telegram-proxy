49673d90dfbd0490fb12e1d1e768153c06de4e75
sentenzo <sentenzo@users.noreply.github.com>
2024-03-24 10:35:26 +0300

    loading ENV variables

--- openai_connect/__init__.py
+++ openai_connect/__init__.py
@@ -1,4 +1,9 @@
+from dotenv import load_dotenv
+
 from .exception import OpenAiConnectException
 from .message import Message, MessageType
+
+load_dotenv(".env.openaiapi")
+load_dotenv(".env.telegram")
 
 __all__ = ["Message", "MessageType", "OpenAiConnectException"]
