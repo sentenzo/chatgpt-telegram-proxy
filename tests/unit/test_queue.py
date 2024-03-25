ffcef966cca1846e63535366e10fffe49581b422
sentenzo <sentenzo@users.noreply.github.com>
2024-03-25 18:34:43 +0300

    tests for `Queue`

--- tests/unit/test_queue.py
+++ tests/unit/test_queue.py
@@ -2,23 +2,17 @@ import asyncio
 
 import pytest
 
-from openai_connect.message import Message, MessageType
 from openai_connect.queue import Queue
 from openai_connect.queue.impl import AsyncIoQueue
+
+from .const import MESSAGES
 
 TIMEOUT = 0.1
 
 
-async def put_messages(q: Queue, n: int, topics: tuple = (None,)) -> None:
-    for i in range(n):
-        m_type = (
-            MessageType.INCOMING,
-            MessageType.OUTGOING,
-            MessageType.UNKNOWN,
-        )[i % 3]
-        message = Message(123, m_type, "some_chat_id")
-        topic = topics[i % len(topics)]
-        await q.put(message, topic)
+async def put_messages(q: Queue) -> None:
+    for message in MESSAGES:
+        await q.put(message)
 
 
 async def test_init() -> None:
@@ -29,29 +23,26 @@ async def test_put() -> None:
     queue = AsyncIoQueue()
 
     await asyncio.wait_for(
-        put_messages(queue, 1000, (None, "a", "b", "c")), TIMEOUT
+        put_messages(queue), TIMEOUT
     )  # can fail with TimeoutError
 
 
 async def test_get() -> None:
     queue = AsyncIoQueue()
 
-    message_in = Message(
-        -14, MessageType.INCOMING, "some_chat_id", message_text="qwerty"
-    )
-    await queue.put(message_in, "some_topic")
-    message_out = await asyncio.wait_for(queue.get("some_topic"), TIMEOUT)
+    message_in = MESSAGES[0]
+    await queue.put(message_in)
+    message_out = await asyncio.wait_for(queue.get(), TIMEOUT)
     assert message_in == message_out
 
-    n = 100
-    await put_messages(queue, n, topics=(None,))
+    await put_messages(queue)
 
-    async def get_messages(k: int) -> None:
-        for _ in range(k):
-            await queue.get(None)
+    async def get_messages(n: int) -> None:
+        for _ in range(n):
+            await queue.get()
 
-    await asyncio.wait_for(get_messages(n), TIMEOUT)
+    await asyncio.wait_for(get_messages(len(MESSAGES)), TIMEOUT)
 
     with pytest.raises(asyncio.TimeoutError):
         # no messages in the queue now
-        await asyncio.wait_for(queue.get(None), TIMEOUT)
+        await asyncio.wait_for(queue.get(), TIMEOUT)
